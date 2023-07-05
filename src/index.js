e9d15f7054038d596c99616f415ad1e238c55c25
SergioMayen <sergio.mayen@ct.uneatlantico.es>
2023-07-05 12:39:49 +0200

    feature: created the backend with firebase and implemented the functions: created, deleted, updated

---
+++ src/index.js
@@ -0,0 +1,86 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, onSnapshot, deleteDoc, doc, addDoc, query, where, orderBy, serverTimestamp, getDoc, updateDoc } from 'firebase/firestore'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCl5TBx0CAnGK8TDOLDvm9ezpNL_8kfOqE",
+    authDomain: "firststep-52c3b.firebaseapp.com",
+    projectId: "firststep-52c3b",
+    storageBucket: "firststep-52c3b.appspot.com",
+    messagingSenderId: "110246368829",
+    appId: "1:110246368829:web:1b7a9cc6fb1cab26597c54"
+};
+
+initializeApp(firebaseConfig)
+
+const db = getFirestore()
+const auth = getAuth()
+
+const colRef = collection(db, 'books')
+
+const q = query(colRef, orderBy('createdAt'));
+
+onSnapshot(q, (snapshot) => {
+    const books = []
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(books)
+})
+
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addDoc(colRef, {
+        tittle: addBookForm.tittle.value,
+        author: addBookForm.author.value,
+        createdAt: serverTimestamp()
+    })
+    .then(() => {
+        addBookForm.reset();
+    })
+})
+
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docRef = doc(db, 'books', deleteBookForm.id.value);
+    deleteDoc(docRef)
+        .then(() => {
+            deleteBookForm.reset();
+        })
+})
+
+const updateBookForm = document.querySelector('.update')
+updateBookForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docRef = doc(db, 'books', updateBookForm.id.value)
+    updateDoc(docRef, {
+        tittle: 'updated tittle'
+    })
+    .then(() => {
+        updateBookForm.reset()
+    })
+
+})
+
+const signupForm = document.querySelector('.signup')
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const email = signupForm.email.value;
+    const password = signupForm.password.value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((cred) => {
+            console.log('user created', cred.user)
+            signupForm.reset()
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+})        
